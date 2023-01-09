a63c405b11fe1f89ea913f2b9c1522e6c74ce310
blabarsgummi <118172095+blabarsgummi@users.noreply.github.com>
2023-01-09 15:37:11 +0100

    програмка для заполнения массива числами
    первое число соответствует длинне массива и каждое последующее уменьшается на единицу

---
+++ array_test/array_test.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<Windows.h>
+
+using namespace std;
+int main()
+{
+	const int N = 50;
+	int array[N];
+	int el = N;
+	for (int i = 0; i < N; i++)
+	{
+		array[i] = el--;
+	}
+}
